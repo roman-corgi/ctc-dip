5e3f2e1331af348571c56e53cc7cf2a2747096b3
Al Niessner <Al.Niessner@xxx.xxx>
2026-04-21 13:16:51 -0700

    add view() to auto-generated dawgie.StateVector children

--- dip/base/__init__.py
+++ dip/base/__init__.py
@@ -2,7 +2,7 @@
 
 from ._fsm import Orchestrator, Runner
 from ._types import Calibration, Configuration, Cpgs, Manifest, Recipe
-from ._wa import ds_asdict, features_asdict
+from ._wa import ds_asdict, features_asdict, generic_view
 from dip.basis.fsm import ProductStatus
 
 __all__ = [
@@ -16,4 +16,5 @@ __all__ = [
     'ProductStatus',
     'ds_asdict',
     'features_asdict',
+    'generic_view',
 ]
