a897e8a078bd6130f9627ea68efe175b811f5135
al-niessner <1130658+al-niessner@users.noreply.github.com>
2026-04-28 13:43:14 -0700

    clean out the caldb (#14)
    * clean out the caldb

--- dip/base/_fsm.py
+++ dip/base/_fsm.py
@@ -163,6 +163,13 @@ class Runner(dip.basis.fsm.AbstractModel):
         if self.__sandbox:
             self.__sandbox.cleanup()
             self.__sandbox = None
+            caldb = Path(os.path.expandvars('${HOME}/.corgidrp'))
+            if caldb.is_dir():
+                for item in caldb.iterdir():
+                    if item.is_dir() and not item.is_symlink():
+                        shutil.rmtree(item)
+                    else:
+                        item.unlink()
 
     def _do_warehouse(self) -> bool:
         '''move the data to SSC storage location'''
