4b0896bf8348588926f142ea9cce5856469b0a55
Al Niessner <Al.Niessner@xxx.xxx>
2026-08-29 15:12:12 -0700

    removed unnecessary channels

--- dip/clerk/__init__.py
+++ dip/clerk/__init__.py
@@ -7,7 +7,6 @@ import dawgie.base
 
 # pylint: disable=duplicate-code
 
-
 def analysis(
     prefix: str, ps_hint: int = 0, runid: int = -1
 ) -> dawgie.FactoryPlaceholder[dawgie.base.Analysis]:
@@ -28,7 +27,6 @@ def task(
     prefix: str, ps_hint: int = 0, runid: int = -1, target: str = '__none__'
 ) -> dawgie.FactoryPlaceholder[dawgie.base.Task]:
     raise NotImplementedError('placeholder until dawgie monkey patches me')
-
 
 # pylint: enable=duplicate-code
 # <-------------  ABOVE: auto generated code above do not edit  ------------->
